6ed5b181dd6a75859237edc03427efd112152f43
andersKallesoee <anderskallesoee@gmail.com>
2021-05-05 13:19:56 +0200

    Revert "final touches"
    This reverts commit ab1d663b7f773b36ab39863f021dfd47523688bd.

--- Microwave.Test.Implementation/IT4_CookControllerTimer.cs
+++ Microwave.Test.Implementation/IT4_CookControllerTimer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Microwave.Classes.Boundary;
 using Microwave.Classes.Controllers;
 using Microwave.Classes.Interfaces;
@@ -6,6 +7,7 @@ using NSubstitute;
 using NSubstitute.ReceivedExtensions;
 using NSubstitute.Routing.Handlers;
 using NUnit.Framework;
+using Timer = Microwave.Classes.Boundary.Timer;
 
 namespace Microwave.Test.Implementation
 {
@@ -50,6 +52,7 @@ namespace Microwave.Test.Implementation
             _cookController.UI = _userInterface;
         }
 
+        // Test of method Start() in Timer, property TimeRemaining in Timer and event handler OnTimerTick() in CookController
         [Test]
         public void StartCancelBtnPress_DisplayShowTime_RecievedCall()
         {
@@ -59,6 +62,37 @@ namespace Microwave.Test.Implementation
             _timeBtn.Press();
             _startCancelBtn.Press();
             _fakeDisplay.Received(1).ShowTime(1,0);
+        }
+
+        // Test of event handler OnTimerExpired() in CookController
+        [Test]
+        public void StartCancelBtnPress_DisplayClear_RecievedCall()
+        {
+            _door.Open();
+            _door.Close();
+            _powerBtn.Press();
+            _timeBtn.Press();
+            _startCancelBtn.Press();
+
+            Thread.Sleep(61000);
+            _fakeDisplay.Received(1).Clear();
+        }
+
+        // Test of method Stop() in Timer
+        [Test]
+        public void StartCancelBtnPress_LightTurnOff_RecievedNoCall()
+        {
+            _door.Open();
+            _door.Close();
+            _powerBtn.Press();
+            _timeBtn.Press();
+            _startCancelBtn.Press();
+            _fakeLight.ClearReceivedCalls();
+
+            _door.Open();
+
+            Thread.Sleep(61000);
+            _fakeLight.Received(0).TurnOff();
         }
     }
 }
